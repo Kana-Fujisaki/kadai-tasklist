a04106b52c0dabe295351c6312f8fb41b9b79ef8
EC2 Default User <ec2-user@ip-172-31-25-135.us-east-2.compute.internal>
2018-06-07 01:25:11 +0000

    TASKLIST DONE

--- database/migrations/2018_06_07_101430_create_tasks_table.php
+++ database/migrations/2018_06_07_101430_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasks extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTasks extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+      Schema::table('tasks', function (Blueprint $table) {
             $table->string('status');
         });
     }
@@ -28,5 +28,6 @@ class CreateTasks extends Migration
         Schema::table('tasks', function (Blueprint $table) {
             $table->dropColumn('status');
         });
+        
     }
 }
